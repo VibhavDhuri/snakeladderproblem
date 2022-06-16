d576d60bb49ceacd64dc465d0e1a7a233fad9009
VibhavDhuri <vibhavpdhuri@gmail.com>
2022-06-17 01:25:29 +0530

    Added: UC1

---
+++ UC1.java
@@ -0,0 +1,10 @@
+package snakeladderproblem;
+
+public class UC1 {
+
+	public static void main(String[] args) {
+		int playerOnePosition = 0;
+		System.out.println("The initial position of player one is: " + playerOnePosition);
+	}
+
+}
